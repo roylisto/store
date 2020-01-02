5122b82de3a20379f5388f7dd5b5e8c0520f9dba
tokanggit <tokang.dev@gmail.com>
2020-01-02 22:20:33 +0700

    add frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,116 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-require('./bootstrap');
+Vue.use(VueRouter)
 
-window.Vue = require('vue');
+import App from './pages/App'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import SingleProduct from './pages/SingleProduct'
+import Checkout from './pages/Checkout'
+import Confirmation from './pages/Confirmation'
+import UserBoard from './pages/UserBoard'
+import Admin from './pages/Admin'
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: '/products/:id',
+            name: 'single-products',
+            component: SingleProduct
+        },
+        {
+            path: '/confirmation',
+            name: 'confirmation',
+            component: Confirmation
+        },
+        {
+            path: '/checkout',
+            name: 'checkout',
+            component: Checkout,
+            props: (route) => ({ pid: route.query.pid })
+        },
+        {
+            path: '/dashboard',
+            name: 'userboard',
+            component: UserBoard,
+            meta: {
+                requiresAuth: true,
+                is_user: true
+            }
+        },
+        {
+            path: '/admin/:page',
+            name: 'admin-pages',
+            component: Admin,
+            meta: {
+                requiresAuth: true,
+                is_admin: true
+            }
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: Admin,
+            meta: {
+                requiresAuth: true,
+                is_admin: true
+            }
+        },
+    ],
+})
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem('bigStore.jwt') == null) {
+            next({
+                path: '/login',
+                params: { nextUrl: to.fullPath }
+            })
+        } else {
+            let user = JSON.parse(localStorage.getItem('bigStore.user'))
+            if (to.matched.some(record => record.meta.is_admin)) {
+                if (user.is_admin == 1) {
+                    next()
+                }
+                else {
+                    next({ name: 'userboard' })
+                }
+            }
+            else if (to.matched.some(record => record.meta.is_user)) {
+                if (user.is_admin == 0) {
+                    next()
+                }
+                else {
+                    next({ name: 'admin' })
+                }
+            }
+            next()
+        }
+    } else {
+        next()
+    }
+})
 
 const app = new Vue({
-    el: '#app',
-});
+  el: '#app',
+  components: {App},
+  router,
+})
